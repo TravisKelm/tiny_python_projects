995febfd44a21909c7f9fcfd792eab63564fc78b
Dr. Travis R. Kelm <tkelm@mail.fresnostate.edu>
2025-05-31 11:44:03 -0700

    broke argparse into own function

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -3,10 +3,13 @@
 
 import argparse
 
-def main():
+def get_args():
     parser = argparse.ArgumentParser(description='This program says hello')
     parser.add_argument('-n', '--name', metavar='name', default='World', help='Whom to greet')
-    args = parser.parse_args()
+    return parser.parse_args()
+    
+def main():
+    args = get_args()
     print('Hello, ' + args.name + '!')
     
 if __name__ == '__main__':
