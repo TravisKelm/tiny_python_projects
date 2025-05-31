282b38abf40a9969bff270afd445eaa29d3f703e
Dr. Travis R. Kelm <tkelm@mail.fresnostate.edu>
2025-05-31 11:38:23 -0700

    Added main() function

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -3,7 +3,11 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='This program says hello')
-parser.add_argument('-n', '--name', metavar='name', default='World', help='Whom to greet')
-args = parser.parse_args()
-print('Hello, ' + args.name + '!')
+def main():
+    parser = argparse.ArgumentParser(description='This program says hello')
+    parser.add_argument('-n', '--name', metavar='name', default='World', help='Whom to greet')
+    args = parser.parse_args()
+    print('Hello, ' + args.name + '!')
+    
+if __name__ == '__main__':
+    main()
